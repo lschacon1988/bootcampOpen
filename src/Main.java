8ea8cbad391882d95b028fac17246600cb093d41
lschacon1988 <lschacon1988@gmail.com>
2022-06-20 15:30:58 -0500

    Update tarea

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,23 @@
 public class Main {
-    public static void main(String[] args){
-        int resultado = suma(5,9,8);
-        System.out.println(resultado);
-      Coche miCoche = new Coche();
-      miCoche.sumarPuerta();
-        System.out.println(miCoche.puerta);
-    }
-    public static int suma(int a, int b, int c){
-        return  a + b + c;
-    }
+  public static void main(String[] args) {
+    int resultado = suma(5, 9, 8);
+    System.out.println(resultado);
+    Coche miCoche = new Coche();
+    miCoche.sumarPuerta();
+    System.out.println("El numero de puertas es: " + miCoche.puerta);
+  }
 
-
+  public static int suma(int a, int b, int c) {
+    return a + b + c;
+  }
 
 }
 
 class Coche {
-    public int puerta = 4;
+  public int puerta = 4;
 
-    public void sumarPuerta(){
-     this.puerta++;
-    }
+  public void sumarPuerta() {
+    this.puerta++;
+  }
 
 }
