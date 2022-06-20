c8d7b6e657062daa4190300cefea728390e2c92b
lschacon1988 <lschacon1988@gmail.com>
2022-06-20 13:57:37 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args){
+        int resultado = suma(5,9,8);
+        System.out.println(resultado);
+      Coche miCoche = new Coche();
+      miCoche.sumarPuerta();
+        System.out.println(miCoche.puerta);
+    }
+    public static int suma(int a, int b, int c){
+        return  a + b + c;
+    }
+
+
+
+}
+
+class Coche {
+    public int puerta = 4;
+
+    public void sumarPuerta(){
+     this.puerta++;
+    }
+
+}
