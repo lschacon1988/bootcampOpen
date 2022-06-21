112b2843be99186c9327355e9de2e469a2c1ec0f
lschacon1988 <lschacon1988@gmail.com>
2022-06-21 13:21:43 -0500

    ejercicio 4

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,23 @@ public class Main {
     Coche miCoche = new Coche();
     miCoche.sumarPuerta();
     System.out.println("El numero de puertas es: " + miCoche.puerta);
+    ////////////////////////////
     int numeroIf = 5;
-  
     extracted(numeroIf);
+    ///////////////////////////
+    int contador = 0;
+    while (contador < 3){
+      System.out.println("contador vale: " + contador);
+      contador++;
+    }
+    do{
+      System.out.println("contador do while vale : " + contador);
+    }while (contador < 0);
+  for (int numeroFor= 0; numeroFor <=3 ; numeroFor++){
+    System.out.println("esta es la variable numeroFor: "+ numeroFor);
+  }
+  ///////////////////////////////
+
   }
   
   private static void extracted(int numeroIf) {
