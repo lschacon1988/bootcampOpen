130eb38c494c0ce0eb58579435b163398908e2f7
lschacon1988 <lschacon1988@gmail.com>
2022-06-22 15:13:16 -0500

    ejercicio tema 8

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
   static Ejercico4 ejercico4 = new Ejercico4();
   static Coche miCoche = new Coche();
+  static Personas persona1 = new Personas();
 
   public static int suma(int a, int b, int c) {
     return a + b + c;
@@ -12,9 +13,16 @@ public class Main {
     ////////////////////////////
     miCoche.sumarPuerta();
     System.out.println("El numero de puertas es: " + miCoche.puerta);
-    ////////////////////////////
+    /////////////ejercico tema 4///////////////
+    // System.out.println(ejercico4);
+    /////////////ejercico tema 8///////////////
 
-    System.out.println(ejercico4);
+    persona1.setEdad(34);
+    persona1.setTelefono(310261882);
+    persona1.setName("luis");
+    System.out.println("Edad: "+ persona1.getEdad());
+    System.out.println("Telefono: "+ persona1.getTelefono());
+    System.out.println("Name: "+ persona1.getName());
   }
 
 }
---
+++ src/Personas.java
@@ -0,0 +1,27 @@
+public class Personas {
+    private int edad;
+    private int telefono;
+    private String name;
+
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+    public void setTelefono(int telefono){
+        this.telefono = telefono;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public int getEdad(){
+        return this.edad;
+    }
+    public int getTelefono(){
+        return this.telefono;
+    }
+    public String getName(){
+        return this.name;
+    }
+
+
+
+}
